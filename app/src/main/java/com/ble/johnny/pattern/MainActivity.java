62ecf35c5757fb06677f1bcacbe1320f1e98a427
johnny <fireiori70807@gmail.com>
2019-06-20 18:52:18 +0800

    composite

--- app/src/main/java/com/ble/johnny/pattern/MainActivity.java
+++ app/src/main/java/com/ble/johnny/pattern/MainActivity.java
@@ -14,6 +14,7 @@ import bridge.Car;
 import bridge.Produce;
 import bridge.Vehicle;
 import builder.McFood;
+import composite.Employee;
 import factory.FactoryPatternDemo;
 import singleton.SingleObject;
 
@@ -59,5 +60,37 @@ public class MainActivity extends AppCompatActivity {
         vehicle1.manufacture();
         Vehicle vehicle2 = new Bike(new Produce(), new Assemble());
         vehicle2.manufacture();
+
+        //composite
+        Employee CEO = new Employee("John","CEO", 30000);
+
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura","Marketing", 10000);
+        Employee clerk2 = new Employee("Bob","Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard","Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        //打印该组织的所有员工
+
+        Log.w("composite",CEO.toString());
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            Log.d("composite",headEmployee.toString());
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
     }
 }
